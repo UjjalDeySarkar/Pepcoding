e4699a708be41f2c545a69218bcd91df395507f7
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-03-17 00:38:37 +0530

    add decreasing recursive function

--- Print Increasing/src/com/ujjal/Main.java
+++ Print Increasing/src/com/ujjal/Main.java
@@ -8,7 +8,10 @@ public class Main {
 	// write your code here
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
+        System.out.println("Increasing");
         inc(n);
+        System.out.println("Decreasing");
+        dec(n);
     }
 
     public static void inc(int n){
@@ -18,4 +21,13 @@ public class Main {
         inc(n-1);
         System.out.println(n);
     }
+
+    public static void dec(int n){
+        if (n==0){
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+
 }
