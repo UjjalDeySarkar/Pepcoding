38942597abe574d877db28d42a2fedf65ba66613
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-03-19 20:00:29 +0530

    Display arrar

---
+++ Display Array using recursion/src/com/ujjal/Main.java
@@ -0,0 +1,23 @@
+package com.ujjal;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        displayarr(arr,0);
+    }
+
+    public static void displayarr(int[]arr, int idx){
+        if (idx== arr.length) return;
+        System.out.println(arr[idx]);
+        displayarr(arr,idx+1);
+    }
+}
